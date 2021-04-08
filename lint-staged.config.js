ef7f87ebddb1483d8f3f0d62446dcdc16651de37
penglinwei <penglinwei@penglinweideiMac.local>
2021-04-08 14:54:56 +0800

    feat: ✨ Modify lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': [
-    'eslint --fix --cache --ext .js,jsx,ts,tsx',
-    'prettier --write'
-  ],
+  '**/*.{js?(x),ts?(x)}': ['eslint --fix --cache', 'prettier --write'],
   // https://stylelint.io/user-guide/usage/options
   '**/*.{css,less,sass}': [
     'stylelint --fix --color --cache --ignore-pattern .stylelintignore'
